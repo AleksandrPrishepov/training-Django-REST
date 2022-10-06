172fd9c08ee8fc029aecff6217a67e904f53f063
AleksandrPrishepov <sanyaprishepov89@gmail.com>
2022-10-06 21:20:54 +0300

    used ModelSerializer

--- drfsite/women/serialisers.py
+++ drfsite/women/serialisers.py
@@ -8,26 +8,30 @@ from .models import Women
 
 
 
-class WomenSerializer(serializers.Serializer):
+class WomenSerializer(serializers.ModelSerializer):
 
-    title = serializers.CharField(max_length=250)
-    content = serializers.CharField()
-    time_create = serializers.DateTimeField(read_only=True)
-    time_update = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField(default=True)
-    cat_id = serializers.IntegerField()
+    class Meta:
+        model = Women
+        fields = "__all__"
 
-    def create(self, validated_data):
-        return  Women.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.content = validated_data.get("content", instance.content)
-        instance.time_update = validated_data.get("time_update", instance.time_update)
-        instance.is_published = validated_data.get("is_published", instance.is_published)
-        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
-        instance.save()
-        return instance
+    # title = serializers.CharField(max_length=250)
+    # content = serializers.CharField()
+    # time_create = serializers.DateTimeField(read_only=True)
+    # time_update = serializers.DateTimeField(read_only=True)
+    # is_published = serializers.BooleanField(default=True)
+    # cat_id = serializers.IntegerField()
+    #
+    # def create(self, validated_data):
+    #     return  Women.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get("title", instance.title)
+    #     instance.content = validated_data.get("content", instance.content)
+    #     instance.time_update = validated_data.get("time_update", instance.time_update)
+    #     instance.is_published = validated_data.get("is_published", instance.is_published)
+    #     instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+    #     instance.save()
+    #     return instance
 
 
 
