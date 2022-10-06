3667d12fe131c01ec3c489586751dc0fe873678a
AleksandrPrishepov <sanyaprishepov89@gmail.com>
2022-10-06 20:46:29 +0300

    used different views

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,7 +1,11 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+
 from rest_framework.generics import ListCreateAPIView
+
+
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,40 +13,49 @@ from .models import *
 from .serialisers import WomenSerializer
 
 
-class WomenAPIView(APIView):
 
-    def get(self, request):
-        lst = Women.objects.all()
-        return Response({'posts': WomenSerializer(lst, many=True).data})
+class WomenAPIView(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'pots': serializer.data})
+class WomenAPIViewUp(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"errors":"Not pk"})
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"errors": "Not pk"})
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'pots': serializer.data})
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs['pk']
-        if pk == None:
-            return Response({"errors": "Not pk"})
-        try:
-            d = Women.objects.get(pk=pk).delete()
-        except:
-            return Response({"errors": "Not pk"})
-        return Response({"pots":"deleted post"+str(pk)})
+    # def get(self, request):
+    #     lst = Women.objects.all()
+    #     return Response({'posts': WomenSerializer(lst, many=True).data})
+    #
+    # def post(self, request):
+    #     serializer = WomenSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response({'pots': serializer.data})
+    #
+    # def put(self, request, *args, **kwargs):
+    #     pk = kwargs.get("pk", None)
+    #     if not pk:
+    #         return Response({"errors":"Not pk"})
+    #     try:
+    #         instance = Women.objects.get(pk=pk)
+    #     except:
+    #         return Response({"errors": "Not pk"})
+    #     serializer = WomenSerializer(data=request.data, instance=instance)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response({'pots': serializer.data})
+
+    # def delete(self, request, *args, **kwargs):
+    #     pk = kwargs['pk']
+    #     if pk == None:
+    #         return Response({"errors": "Not pk"})
+    #     try:
+    #         d = Women.objects.get(pk=pk).delete()
+    #     except:
+    #         return Response({"errors": "Not pk"})
+    #     return Response({"pots":"deleted post"+str(pk)})
+
 
 
 
